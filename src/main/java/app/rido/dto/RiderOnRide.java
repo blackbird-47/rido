9bdfdf108495bf0b0ae01e2b464756c092346477
Manish Patti <manish.patti@tesco.com>
2023-07-09 19:03:00 +0530

    dto refactor

--- src/main/java/app/rido/dto/RiderOnRide.java
+++ src/main/java/app/rido/dto/RiderOnRide.java
@@ -2,8 +2,13 @@ package app.rido.dto;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class RiderOnRide {
     private String rideId;
     private String riderId;
+    private boolean noshow;
+    private RiderIntent riderIntent;
+    private Date lastModifiedDate;
 }
