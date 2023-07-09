7137f0a2c17e80bb04a6021d177158ddb061af3c
Manish Patti <manish.patti@tesco.com>
2023-07-09 20:38:23 +0530

    entity annotation

--- src/main/java/app/rido/dto/Rider.java
+++ src/main/java/app/rido/dto/Rider.java
@@ -1,11 +1,13 @@
 package app.rido.dto;
 
+import jakarta.persistence.Entity;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 
 import java.util.Date;
 
 @Data
+@Entity
 public class Rider {
     @Id
     private String id;
