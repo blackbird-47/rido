15a2b89b6b57b90bde572854ae2e999f3f306a81
IN22912944 <swastik.gupta@tesco.com>
2023-07-09 16:26:20 +0530

    Project setup

---
+++ src/test/java/app/rido/rido/RidoApplicationTests.java
@@ -0,0 +1,13 @@
+package app.rido.rido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
